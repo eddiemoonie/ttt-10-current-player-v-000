ddc5fd4ac8a2b4d5ee0502c18fec2fbf17684a4a
Eddie Moon <eddiemoonie@gmail.com>
2018-01-23 21:53:42 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,13 +9,21 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).odd?
-    "O"
-  elsif turn_count(board).even?
+  if turn_count(board) % 2 == 0
     "X"
+  else
+    "O"
   end
 end
 
 #def current_player(board)
-# => turn_count(board) % 2 == 0 ? "X" ? "O"
+  #if turn_count(board).odd?
+    #{}"O"
+  #elsif turn_count(board).even?
+    #{}"X"
+  #end
+#end
+
+#def current_player(board)
+  #turn_count(board) % 2 == 0 ? "X" ? "O"
 #end
